b55bc66267229564a2c39d520348fbf609862767
Knar <qenpoghos@gmail.com>
2018-10-06 15:14:02 +0100

    Updates

---
+++ precache-manifest.51f996056859cfc8071a78ff590fc2d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "177e1b728fb0d4ffdfea",
+    "url": "/weather/static/js/runtime~main.177e1b72.js"
+  },
+  {
+    "revision": "f1491391ea5418b8f3ac",
+    "url": "/weather/static/js/main.f1491391.chunk.js"
+  },
+  {
+    "revision": "237fbed24773703c9ff7",
+    "url": "/weather/static/js/1.237fbed2.chunk.js"
+  },
+  {
+    "revision": "f1491391ea5418b8f3ac",
+    "url": "/weather/static/css/main.82bfb3aa.chunk.css"
+  },
+  {
+    "revision": "8c5b03a941d7e81203ae8c8974e6bc3e",
+    "url": "/weather/index.html"
+  }
+];
