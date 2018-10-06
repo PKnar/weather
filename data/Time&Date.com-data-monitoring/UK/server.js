10a6925422ef1fec833bfd61fdfceb87ab34d256
Knar <qenpoghos@gmail.com>
2018-10-06 14:45:30 +0100

    First pages commit

---
+++ data/Time&Date.com-data-monitoring/UK/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+const requireDir = require("require-dir");
+const dir = requireDir("./stats-folder");
+
+app.get("/data", (req, res) => {
+  res.send({ dir });
+});
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
